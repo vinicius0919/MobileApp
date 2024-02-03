154d3be895ae4277f23ec884f2d300007ded8846
vinicius0919 <viniciuslima0919@gmail.con>
2024-02-03 18:02:23 -0300

    New design, panels, new methods,notifications

--- app/src/main/java/com/example/adondeapp/MainActivity.java
+++ app/src/main/java/com/example/adondeapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.adondeapp;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,6 +19,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
     private Context context = this;
     Switch switcher = new Switch(getSupportFragmentManager());
 
@@ -24,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         changeFragment(new StartFragment());
+
+        // Verificar se a permissão já foi concedida
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            NotificationHelper notificationHelper = new NotificationHelper();
+            notificationHelper.createNotification(context, "ADonde em execução", "Há produtos deliciosos perto de você, confira!");  // 'this' é um contexto válido, geralmente referindo-se à atividade ou aplicativo
+
+        } else {
+            // Solicitar a permissão
+            requestNotificationPermission();
+        }
+
     }
     public void changeFragment(Fragment fragment) {
         FragmentManager fm = getSupportFragmentManager();
@@ -33,6 +50,26 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    private void requestNotificationPermission() {
+        // Solicitar a permissão de notificação
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == NOTIFICATION_PERMISSION_CODE) {
+            // Verificar se a permissão foi concedida
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissão concedida, execute a lógica relacionada às notificações aqui
+                NotificationHelper notificationHelper = new NotificationHelper();
+                notificationHelper.createNotification(context, "ADonde em execução", "Há produtos deliciosos perto de você, confira!");  // 'this' é um contexto válido, geralmente referindo-se à atividade ou aplicativo
+            } else {
+                // Explique ao usuário que a funcionalidade de notificação pode ser limitada sem a permissão
+            }
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/example/adondeapp/NotificationHelper.java
@@ -0,0 +1,62 @@
+package com.example.adondeapp;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationHelper {
+
+    private final String CHANNEL_ID = "09";
+
+    public void createNotification(Context context, String titulo, String texto) {
+        // Verifica se é necessário criar o canal de notificação (apenas para versões mais recentes do Android)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(context);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo2)
+                .setContentTitle(titulo)
+                .setContentText(texto)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            //  Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(1, builder.build());
+
+        // Delay de 5 segundos para cancelar a notificação
+        //new Handler().postDelayed(() -> {
+        //    notificationManager.cancel(1);
+        //}, 5000);
+    }
+
+    private void createNotificationChannel(Context context) {
+        CharSequence name = "Canal de Notificação";
+        String description = "Canal padrão para notificações";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
