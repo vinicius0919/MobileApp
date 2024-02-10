2c424c0345b488d51edb179210504207da77440f
mtsteo <mateusteotonho@gmail.com>
2024-02-09 22:29:51 -0300

    GPS OK!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/adondeapp/MapActivity.kt
+++ app/src/main/java/com/example/adondeapp/MapActivity.kt
@@ -1,51 +1,106 @@
 package com.example.adondeapp
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MapActivity : AppCompatActivity() {
-    private var searchView: SearchView? = null
 
-    /*capitão poço LT LG -1.7443707,-47.0646394*/
-    private val places = ArrayList<Place>()
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val switcher = Switch(supportFragmentManager)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        switcher.changeFragment(MapsFragment())
+        setContentView(R.layout.fragment_maps)
+
+
+        var mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+
+        mapFragment.getMapAsync { map ->
+            googleMap = map
+            enableMyLocation()
+        }
+
+        var buttonMenu : FloatingActionButton = findViewById(R.id.floatingActionButton)
+        buttonMenu.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            googleMap.isMyLocationEnabled = true
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    location?.let {
+                        val latLng = LatLng(location.latitude, location.longitude)
+                        googleMap.addMarker(MarkerOptions().position(latLng).title("My Location"))
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                    }
+                }
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
     }
 
     override fun onRestart() {
         super.onRestart()
         finish()
-        switcher.changeFragment(MapsFragment())
+    }
+    override fun onResume() {
+        super.onResume()
+
     }
 
-    fun AddPonteiro() {
-        searchView = findViewById(R.id.searchView)
-        val mapFragment =
-            supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
-        mapFragment?.getMapAsync { googleMap: GoogleMap -> addMarker(googleMap) }
+    override fun onPause() {
+        super.onPause()
 
-        // Adiciona um exemplo de local
-        places.add(Place("Google", LatLng(-1.7443707, -47.0646394), "Capitão Poço", 4.8f))
     }
 
-    private fun addMarker(googleMap: GoogleMap) {
-        for (place in places) {
-            googleMap.addMarker(
-                MarkerOptions()
-                    .title(place.name)
-                    .snippet(place.address)
-                    .position(place.latLng)
-            )
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+    }
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    }
+
 }
 
-internal class Place(val name: String, val latLng: LatLng, val address: String, val rating: Float)
+data class Place(
+    val latLng: LatLng,
+
+)
